eab0970ce3d5a81e36af0ad80245ae7a8f05a973
Dylan DUBOIS <dylan@dylan-dubois.fr>
2025-04-30 14:03:22 +0200

    Add : SelfSigned certificates

---
+++ OnPremise/Private-Certificate/Generate-Certificate.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [Alias("FilePath")]
+    [parameter(Mandatory=$true)][string]$DnsName
+)
+
+# Create certificate
+$mycert = New-SelfSignedCertificate -DnsName $DnsName -CertStoreLocation "cert:\CurrentUser\My" -NotAfter (Get-Date).AddYears(1) -KeySpec KeyExchange
+
+# Export certificate to .pfx file
+$mycert | Export-PfxCertificate -FilePath "..\..\Ressources\$($DnsName.Split(".")[0]).pfx" -Password (Get-Credential).password
+
+# Export certificate to .cer file
+$mycert | Export-Certificate -FilePath "..\..\Ressources\$($DnsName.Split(".")[0]).cer"
